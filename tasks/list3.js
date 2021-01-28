e05f07bd13d08f6bf56a19fa12f7833bf83ca415
zabalueva <age_eva@mail.ru>
2021-01-28 13:54:19 +0300

    feat: codewars kata array

--- tasks/list3.js
+++ tasks/list3.js
@@ -1,29 +1,76 @@
 
 /* function isAnagram(test, original) {
-  test = test.toLowerCase().split('');
-	original = original.toLowerCase().split('');
-	let finded;
-	if (test.length == original.length) {
-		for (let i = 0; i <= test.length; i++) {
-			original.includes(test[i]) ?
 
+	let findedTesty = {};
+	let findedOriginally = {};
 
+	test.toLowerCase().split('').forEach(element => {
+			findedTesty[element] = findedTesty[element] + 1 || 1;
+		});
+		original.toLowerCase().split('').forEach(element => {
+			findedOriginally[element] = findedOriginally[element] + 1 || 1;
+		});
+		if (Object.keys(findedTesty).length !== Object.keys(findedOriginally).length){
+			return false;
 
-			function finder() {
-				original.includes(test[i]);
-			}
-			finded = original.filter(finder);
-			if (finded.length == 1) return true;
-		}
 	}
-  return false;
-}; */
 
-/* for (let i=0; i <= test.length; i++){
-     original.indexOf(test[i]) == -1
-		} */
+		for (let char in findedTesty) {
+			console.log(char)
+			if (findedTesty[char] !== findedOriginally[char]) {
+				return false;
+			}
 
-/* function mygcd(x, y) {
+			}
+
+
+	return true;
+};
+
+
+console.log(isAnagram("ound", "round")) */
+
+
+
+/*
+const buildCharObject = str => {
+  const charObj = {}
+  for(let char of str.replace(/[^\w]/g).toLowerCase()) {
+    // Если объект уже содержит пару ключ-значение равную значению в цикле,
+    // увеличиваем значение на 1, в противном случае добавляем букву в качестве ключа
+    // и 1 в качестве значения
+    charObj[char] = charObj[char] + 1 || 1
+  }
+  return charObj
+}
+// Главная функция
+const anagram = (strA, strB) => {
+  // Создаём объект для хранения strA
+  const aCharObject = buildCharObject(strA)
+  // Создаём объект для хранения strB
+  const bCharObject = buildCharObject(strB)
+  // Сравниваем количество ключей в обоих объектах
+  // (анаграммы должны иметь одинаковое количество букв)
+  if(Object.keys(aCharObject).length !== Object.keys(bCharObject).length) {
+    return false
+  }
+  // Если оба объекта имеют одинаковое количество ключей, мы можем быть уверены,
+  // что обе строки имеют одинаковое количество символов. Теперь мы можем сравнить
+  // оба объекта, чтобы увидеть, имеют ли они одинаковые буквы в одинаковом количестве
+  for(let char in aCharObject) {
+    if(aCharObject[char] !== bCharObject[char]) {
+      return false
+    }
+  }
+  // Если проверка успешна, строки являются анаграммами — возвращаем true
+  return true
+}
+
+
+console.log(anagram('роз', 'Зор')) */
+
+
+function mygcd(x, y) {
 	let targ;
 	let result;
 
@@ -38,6 +85,7 @@
 			x %= y
 		} else { y %= x };
 	}
+	
 
 
 	return result;
@@ -57,8 +105,8 @@ function NOD (x, y) {
   return x;
 }
 
-console.log(NOD(10, 3))
- */
+console.log(NOD(9, 3))
+
 /*
 function factorial1(n){
 	let result = 1;
@@ -117,7 +165,7 @@ console.log(distinct([1,1,2,4,5,4,4,4,3])) */
 console.log(positiveSum([1,1,2, -1])) */
 
 
-function capital(capitals){
+/* function capital(capitals){
   let result = [
     `The capital of ${capitals[0].state || capitals[0].country} is ${capitals[0].capital}`
     ]
@@ -151,4 +199,79 @@ mixed_capitals = [
 		country: 'Spain',
 		"capital": "Madrid"
 	}]
+ */
+/* const arr = [
+	{name: 'width', value: 10},
+	{name: 'height', value: 20}
+ ]
 
+
+function getObj(arr) {
+	const obj = {};
+
+	arr.forEach(item =>	obj[item.name] = item.value	);
+
+	return obj;
+}
+
+
+console.log(getObj(arr))
+
+
+console.log(typeof (function(){}()))
+ */
+
+
+
+/* console.log(new Array.prototype.constructor(1, 2))
+
+let a = (1,5 - 1) * 2;
+
+console.log(a); */
+
+/* function fizzbuzz(n)
+{ let result = [];
+  for (let i = 1; i < n; i++){
+    if (i % 3 == 0) {
+     result.splice(i, 1, "Fizz")
+    } else if (i % 5 == 0) {
+      result.splice(i, 1, "Buzz")
+    } else result.push(i);
+  }
+ return result;
+
+}
+
+
+console.log(fizzbuzz(5))
+ */
+
+/* function findShort(s){
+  let words = s.split(" ");
+	let shortest = words[0].length;
+	words.forEach(element => {
+		if (element.length < shortest) {
+			shortest = element.length;
+		}
+	});
+
+	return shortest;
+}
+
+console.log(findShort("My love i world")) */
+
+/* function squareDigits(num) {
+	let result = [];
+	for (let i = 0; i < num.toString().length; i++){
+		result.push(num.toString()[i] * num.toString()[i]);
+	}
+	return result.join("");
+}
+
+console.log(squareDigits(456))
+
+function squareDigitals(numb) {
+	return numb.toString().split("").map(item => item*item).join("");
+}
+
+console.log(squareDigitals(45666)) */
