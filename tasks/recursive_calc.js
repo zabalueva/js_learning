d5312007b21dc5f52b1d57fb3a818887c111b481
zabalueva <age_eva@mail.ru>
2021-02-13 12:00:10 +0300

    resursive complete

--- tasks/recursive_calc.js
+++ tasks/recursive_calc.js
@@ -1,23 +1,46 @@
 class DepthCalculator {
-	depth = 1;
-	calculateDepth(arr) {
+	constructor() {
+		this.calculateDepth = this.calculateDepth.bind(this)
+	}
 
-		arr.forEach((item) => {
-			if (Array.isArray(item)) {
-				this.depth++;
-				this.calculateDepth(item)
+	calculateDepth(arr) {
+		if (arr instanceof Array) {
+			let result = 0;
+			if (arr == []) {
+				result = 1;
 			}
-		});
-		return this.depth;
+			return 1 + result + Math.max(...arr.map((item) => this.calculateDepth(item)), result);
+		} else return 0;
 	}
 }
 
-console.log(new DepthCalculator())
-
 const depthCalc = new DepthCalculator();
 
-console.log(depthCalc.calculateDepth([[[]]]))// => 1
+console.log(depthCalc.calculateDepth([[[[]]]]))// => 2
 
-/* depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
+/* console.log(depthCalc.calculateDepth([1, 2, 3, [4, 5]]))//=> 2 */
 
-depthCalc.calculateDepth([[[]]]) => 3 */
+console.log(depthCalc.calculateDepth([1, 2, 3, [8, [2]], 4, 5, []]) )//=> 3
+
+console.log(depthCalc.calculateDepth([1, [8, [2, 3, [3,[]]]], 2, 3, [8, [], [4, [4, [6,[]]]]], 4, 5, []]))
+
+
+//2 failing
+
+/* constructor() {
+	this.depth = 1;
+	this.maxDepth = 0;
+}
+
+calculateDepth(arr) {
+	let result = 1;
+	arr.map((item) => {
+		if (Array.isArray(item)) {
+			return result = 1 + this.calculateDepth(item)
+		}
+	}
+
+	)
+	return result;
+
+} */
