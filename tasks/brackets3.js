69f07e36727a2426d5dd53f725430353441d2a35
zabalueva <age_eva@mail.ru>
2021-03-15 17:01:49 +0300

    feat: solution for brackets

--- tasks/brackets3.js
+++ tasks/brackets3.js
@@ -1,46 +1,48 @@
 function check(str, bracketsConfig) {
 	let bracketsConfigAll = [];
-    let bracketsConfigAllList = [];
-    let stack = [];
+	let bracketsConfigAllList = [];
+	let stack = [];
 
-	for (let i = 0; i < bracketsConfig.length; i++) {
-		bracketsConfigAll[i] = bracketsConfig[i];
-		bracketsConfigAllList = bracketsConfigAll.toString().split(",");
-	}
+for (let i = 0; i < bracketsConfig.length; i++) {
+	bracketsConfigAll[i] = bracketsConfig[i];
+	bracketsConfigAllList = bracketsConfigAll.toString().split(",");
+}
 
-	for (let item of str) {
-		let bracketsIndex = bracketsConfigAllList.indexOf(item);
+/* console.log(str);
+console.log(brackets); */
+
+for (let item of str) {
+	let bracketsIndex = bracketsConfigAllList.indexOf(item);
+
+	if (bracketsIndex % 2 == 0) {
 
 		if (bracketsConfigAllList[bracketsConfigAllList.indexOf(item)]
 			== bracketsConfigAllList[bracketsConfigAllList.indexOf(item) + 1]) {
-			console.log('l')
+			/* console.log('same');
+			console.log(stack[stack.length - 1])
+			console.log(bracketsIndex) */
+			if (stack[stack.length - 1] == bracketsIndex  || stack[stack.length - 1] == bracketsIndex + 1) {
+			/* 	console.log('l') */
+				stack.pop();
+			} else stack.push(bracketsIndex + 1);
 
-		/* 	if ((str.indexOf(item) - str.lastIndexOf(item)) % 2 == 0) {
-				return false;
-			} */
-			if (stack.includes(bracketsIndex + 1)) {
-				console.log('d')
-				stack.pop(bracketsIndex + 1);
-			}
-			stack.push(bracketsIndex + 1);
-			console.log(stack)
+		} else stack.push(bracketsIndex + 1);
 
-		}
 
-		else if (bracketsIndex % 2 == 0) {
-			stack.push(bracketsIndex + 1);
-		} else {
-			if (stack.pop() !== bracketsIndex) {
-				console.log('f')
-				return false;
-			}
-		}
 	}
 
-return stack.length === 0;
+	else {
+
+			if (stack.pop() !== bracketsIndex) {
+				return false;
+			}
+	}
+}
+	return stack.length === 0;
+
 	}
 
 
 //false
 /* console.log(check('877878887777777888888887777777887887788788887887777777788888888887788888', [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']])) */
-console.log(check('88', [ ['8', '8']]))
+console.log(check('(|)(|)', [ ['|', '|'], ['(', ')']]))
--- tasks/codewars4.js
+++ tasks/codewars4.js
@@ -1,4 +1,4 @@
-function inter(s1, s2) {
+/* function inter(s1, s2) {
 	let res = []
 	res.push(...s1, ...s2);
 	let res2 = [];
@@ -17,10 +17,84 @@ A = new Set([1,2]);
 B = new Set([2,3]);
 
 C = inter(A,B)
+console.log(C)*/
 
 
 /* function inter(s1, s2){
   return new Set( [...s1].filter( e => s2.has(e) ) )
 } */
 
-console.log(C)
+
+/* function nthFibo(n) {
+	let res = [0 , 1];
+	for (let i = 1; i < n; i++) {
+		res.push(res[i] + res[i - 1]);
+	}
+	return res[n-1];
+}
+console.log(nthFibo(4)) */
+
+
+/* function divCon(x){
+  let sum1 = 0;
+  let sum2 = 0;
+  x.forEach((item)=>{
+    typeof(item) == 'number' ? sum1 += item :
+    sum2 += +item;
+	})
+    return sum1 - sum2;
+}
+console.log(divCon([9, 3, '7', '3'])) */
+
+/* A.sort();
+  return A[0] !== A[1] || A.length == 1 ? A[0] : A [A.length - 1] */
+
+/* function findOdd(A) {
+	let stack = [];
+	A.sort();
+
+	for (let i = 0; i < A.length - 1; i++) {
+		stack.push(A[i]);
+		if (A[i + 1] == A[i]) {
+			console.log('d')
+			stack.pop();
+		}
+	}
+console.log(stack)
+} */
+/* function findOdd(A) {
+	return Object.assign(A.sort())
+}
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5 ])) */
+
+
+
+/* function findOutlier(int) {
+	let res;
+
+	for (let i = 0; i < int.length; i++) {
+		if (int[0] % 2 == 0 && int[1] % 2 == 0) {
+			if (int[i] % 2 != 0) {
+				res = int[i];
+			}
+		}
+		else if (int[0] % 2 !== 0 && int[1] % 2 !== 0) {
+			if (int[i] % 2 == 0) {
+				res = int[i];
+			}
+		} else  {
+			if (int[2] % 2 !== 0) {
+				if (int[i] % 2 == 0) {
+					res = int[i];
+				}
+			} else if (int[2] % 2 == 0) {
+				if (int[i] % 2 !== 0) {
+					res = int[i];
+				}
+			}
+		}
+	}
+	return res;
+}
+
+console.log(findOutlier([2,6,8,10,3])) */
